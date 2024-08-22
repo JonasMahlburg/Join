0a7eb2d5163cd3349021942bbb4c7862acc77918
Jonas Mahlburg <darrylmanga@yahoo.com>
2024-08-22 16:03:02 +0200

    set reset for Database

--- assets/script/data.js
+++ assets/script/data.js
@@ -1,7 +1,7 @@
 let user = [];
 let contacts = [];
 let tasks = [];
-const BASE_URL = 'https://join-61eb9-default-rtdb.europe-west1.firebasedatabase.app/';
+const BASE_URL = 'https://join-jonas-default-rtdb.europe-west1.firebasedatabase.app/';
 loadData();
 
 async function loadData() {
